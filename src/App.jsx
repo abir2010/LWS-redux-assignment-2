d3ddaa32bdcf06e007cb7614845cad9bef218a79
abir2010 <kaiserabir222@gmail.com>
2023-12-06 23:57:37 +0600

    booking and bookinglist completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header/Header";
+import Booking from "./components/Section/Booking";
+import BookingList from "./components/Section/BookingList";
 import "./styles.css";
 
 function App() {
     return (
         <div>
             <Header />
+            <Booking />
+            <BookingList />
         </div>
     );
 }
